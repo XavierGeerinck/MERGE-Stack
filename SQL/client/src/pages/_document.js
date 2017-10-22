fe2c1bfeb5ca814600124768708b8085f9945432
Xavier Geerinck <thebillkidy@gmail.com>
2017-10-22 15:01:39 +0200

    Add new Document.js

--- SQL/client/src/pages/_document.js
+++ SQL/client/src/pages/_document.js
@@ -1,57 +1,67 @@
-import Document, { Head, Main, NextScript } from 'next/document'
-import flush from 'styled-jsx/server'
+import React from "react";
+import Document, { Head, Main, NextScript } from "next/document";
+import flush from "styled-jsx/server";
 
 export default class MyDocument extends Document {
-    static getInitialProps({ renderPage }) {
-        const { html, head } = renderPage()
-        const styles = flush()
-        return { html, head, styles }
-    }
+  static async getInitialProps({ req, renderPage }) {
+    // Req includes localeDataScript, availableLocales, locale
+    const { html, head, errorHtml, chunks } = renderPage();
+    const styles = flush();
+    return { html, head, errorHtml, chunks, styles };
+  }
 
-    render() {
-        return (
-            <html>
-                <Head>
-                    <title>Instagram Clone</title>
-                    <style jsx global>{`
-                    @import 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800';
-                    
-                    html {
-                        height: 100%;
-                        box-sizing: border-box;
-                    }
+  render() {
+    const script = `window.ENV = '${process.env.ENV || "development"}';`;
 
-                    *, *:before, *:after {
-                        box-sizing: inherit;
-                    }
+    return (
+      <html lang="en">
+        <Head>
+          <script dangerouslySetInnerHTML={{ __html: script }} />
 
-                    a {
-                        -webkit-tap-highlight-color: rgba(0,0,0,0);
-                    }
+          <title>Instagram Clone</title>
+          <style jsx global>{`
+            @import 'https://fonts.googleapis.com/css?family=Open+Sans:300, 400, 700, 800';
 
-                    body {
-                        position: relative;
-                        min-height: 100%;
-                        margin: 0;
-                        padding-bottom: 6rem;
-                        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Open Sans", sans-serif;
-                        text-rendering: optimizeLegibility;
-                    }
+            html {
+              height: 100%;
+              box-sizing: border-box;
+            }
 
-                    html, body {
-                        background-color: #fafafa;
-                        color: #000;
-                    }
-                    `}</style>
-                </Head>
-                <body>
-                    {this.props.customValue}
-                    <Main />
-                    <NextScript />
-                </body>
-            </html>
-        )
-    }
+            *,
+            *:before,
+            *:after {
+              box-sizing: inherit;
+            }
+
+            a {
+              -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+            }
+
+            body {
+              position: relative;
+              min-height: 100%;
+              margin: 0;
+              padding-bottom: 6rem;
+              font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+                "Helvetica Neue", "Open Sans", sans-serif;
+              text-rendering: optimizeLegibility;
+            }
+
+            html,
+            body {
+              background-color: #fafafa;
+              color: #000;
+            }
+          `}</style>
+
+          {this.props.head}
+        </Head>
+        <body>
+          {this.props.customValue}
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
 }
-
- 
